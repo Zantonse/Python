8b9a4a41ea182ee253eea4b33d0725cb4f7f5fa0
CraigV <Craigv@ami.com>
2014-06-03 18:31:28 -0400

    CSV to JSON script
    Format: >> python csvReader.py Ramesh.csv
    
    outputs .json file

---
+++ CSVreader/csvReader.py
@@ -0,0 +1,28 @@
+import csv
+import json
+import sys
+
+fieldnames =["Tennis Lessons","Stroke","Comments"]
+
+def convert(file):
+	# Get the CSV filename and open the file
+	filename = file[0]
+	print "Reading File: " ,filename
+	with open(filename, 'r') as f:
+		reader = csv.reader(f)
+		csv_reader = csv.DictReader(f,fieldnames)
+		
+	# JSON
+		json_filename = filename.split(".")[0]+".json"
+		with open(json_filename, 'wb') as jsonf:
+			data = json.dumps([r for r in csv_reader])
+			jsonf.write(data)
+	
+	# Close files
+			f.close()
+			jsonf.close()
+	print "Save JSON file: ", json_filename
+	
+if __name__=="__main__":
+	convert(sys.argv[1:])
+	
